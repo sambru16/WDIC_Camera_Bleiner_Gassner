ec38abd4d113f6a4f0819dd8c81ab3642d2b6a0f
Lukas Gassner <lukas.gassner@student.htl-rankweil.at>
2021-03-09 08:31:54 +0100

    Kommentare

--- camera_control.py
+++ camera_control.py
@@ -1,39 +1,52 @@
-from picamera import PiCamera 
-from gpiozero import Button
-import datetime as dt
-import os
+from picamera import PiCamera           #Kamera 
+from gpiozero import Button             #GPIO Button
+import datetime as dt                   #Datum und Uhrzeit
+import os                               #Verzeichnis und Dateiverwaltung
 
-button1 = Button(17)
-buttton2 = Button(27)
+button1 = Button(17)                    #Button Zuweisung
+buttton2 = Button(27)           
 button3 = Button(22)
-camera = PiCamera()
+camera = PiCamera()                     #Kamera Zuweisung
 
-videostate = False
+videostate = False                      #Status der Videoaufnahme
 
-camera.start_preview()
+camera.start_preview(alpha=220)         #Kameravorschau aktivieren
 
-def take_picture():
-    pass
+def take_picture():                     #Erste Routine(Bild aufnehmen)
+    try:
+        os.remove('/home/pi/Documents/Camera_Project/rpi_image.jpg')                 #Wenn Bild vorhanden --> löschen
+    except OSError:
+        pass
 
-def control_video():
-    if not videostate:
+    camera.capture('/home/pi/Documents/Camera_Project/rpi_image.jpg')                #Bild aufnehmen und abspeichern
+
+def control_video():                    #Zweite Routine(Video aufnehmen)
+    global videostate           
+    if not videostate:                  #Falls Videoaufnahme nicht aktiv ist --> starten der Aufnahme
         try:
-            os.remove('rpi_video.h264')
+            os.remove('/home/pi/Documents/Camera_Project/rpi_image.jpg')    #Wenn Video vorhanden --> löschen
         except OSError:
             pass
 
-        videostate = True
-        camera.start_recording('rpi_video.h264')
-        #camera.wait_recording(60)
+        videostate = True       
+        camera.start_recording('/home/pi/Documents/Camera_Project/rpi_image.jpg')     #Starten der Aufnahme
+        camera.wait_recording(60)
     
-    if videostate:
-        videostate = False
+    if videostate:                      #Falls Videoaufnahme aktiv --> Aufnahme beenden
+        videostate = False      
         camera.stop_recording()
 
-def picture_overlay():
-    pass
+def picture_overlay():                  #Dritte Routine(Bildbeschriftung)
+    try:
+        os.remove('/home/pi/Documents/Camera_Project/rpi_image.jpg')                  #Wenn Bild vorhanden --> löschen
+    except OSError:
+        pass
 
-while True:
+    camera.annotate_text_size = 110     #Textgröße ändern
+    camera.annotate_text = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')            #Datum und Uhrzeit hinzufügen
+    camera.capture('/home/pi/Documents/Camera_Project/rpi_image.jpg')                 #Bild aufnehmen
+
+while True:                             #GPIO Button Abfrage
     if button1.is_pressed:
         take_picture
     elif buttton2.is_pressed:
